781fee91f63f50fe152dd0beaaa3993ece09146c
sskkfmnn <sskkfmnn@gmail.com>
2023-02-17 13:08:17 +0100

    Lev1_11

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+console.log("");
+
+let x = "Hello";
+let y = "World"
+let z = x + " " +y;
+console.log (z);
+
+document.write(z);
+
+let n = x + " " + y;
+
+document.write (n);
+
+console.log (x + "World");
+
+document.write (x);
+
+let meinString = "Ich bin Erster";
+console.log (meinString + " " + "Ich komme auf Platz zwei");
